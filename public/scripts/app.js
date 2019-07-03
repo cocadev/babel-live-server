13a2309fb8432f957985ef6563421be476efb495
Wang Harry <40970351+funnyjerry@users.noreply.github.com>
2019-07-03 10:17:30 +0800

    renderApp

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,27 +19,30 @@ function getContent(content) {
         return React.createElement(
             'p',
             null,
-            '[Under Construction...]'
+            '[Wang Harry Construction...]'
         );
     }
 }
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h2',
-        null,
-        site.title
-    ),
-    React.createElement(
-        'p',
-        null,
-        site.description
-    ),
-    getContent(site.content)
-);
-
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderApp = function renderApp() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h2',
+            null,
+            site.title
+        ),
+        React.createElement(
+            'p',
+            null,
+            site.description
+        ),
+        getContent(site.content)
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+renderApp();
--- src/app.js
+++ src/app.js
@@ -1,27 +1,30 @@
 console.log('grokonez.com');
- 
+
 // JSX
 const site = {
     title: 'Java Sample Approach',
     description: 'Java/Javascript Technology - Spring Framework'
 };
- 
+
 function getContent(content) {
     if (content) {
         return <p>{content}</p>
     } else {
-        return <p>[Under Construction...]</p>
+        return <p>[Wang Harry Construction...]</p>
     }
 }
- 
-const template = (
-    <div>
-        <h2>{site.title}</h2>
-        <p>{site.description}</p>
-        {getContent(site.content)}
-    </div>
-);
- 
+
 const appRoot = document.getElementById('app');
- 
-ReactDOM.render(template, appRoot);
+
+const renderApp = () => {
+    const template = (
+        <div>
+            <h2>{site.title}</h2>
+            <p>{site.description}</p>
+            {getContent(site.content)}
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+}
+
+renderApp();
